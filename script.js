4312814c477e174b7fd07fb28032c401d9ce4ffd
Ella Braun <ellafbraun@gmail.com>
2026-09-10 17:48:35 +0100

    setup and fake books

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const library = document.querySelector("#library");
+const form = document.querySelector("#form");
+
+const newBook = document.createElement("button");
+const title = document.createElement("h1");
+const popUp = document.createElement("dialog");
+
+library.classList.add("library");
+newBook.classList.add("new-book");
+title.classList.add("title");
+popUp.classList.add("pop-up");
+
+title.textContent = "Ella's Library";
+newBook.textContent = "New Book!";
+
+form.appendChild(title);
+form.appendChild(newBook);
+form.appendChild(popUp);
+
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
@@ -8,9 +27,38 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function() {
     if(this.read) {
-        console.log(`${this.title} by ${this.author}, ${this.pages}, read`);
+        return `${this.title} by ${this.author}, ${this.pages}, read`;
     } else {
-        console.log(`${this.title} by ${this.author}, ${this.pages}, not yet read`);
+        return `${this.title} by ${this.author}, ${this.pages}, not yet read`;
     }
   };
 }
+
+function AddBook(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    book.id = crypto.randomUUID();
+    myLibrary.push(book);
+}
+
+function DisplayBooks() {
+    for(let i=0; i<myLibrary.length; i++) {
+        const book = myLibrary[i];
+
+        const bookDisplay = document.createElement("div");
+
+        bookDisplay.innerHTML = `
+        <p>${book.info()}</p>
+        `;
+
+        bookDisplay.classList.add("book-card");
+        library.appendChild(bookDisplay);
+    }
+}
+
+const myLibrary = [];
+
+AddBook("The Hobbit", "J.R.R. Tolkien", 295, false);
+AddBook("Harry Potter", "J.K. Rowling", 400, true);
+AddBook("Red Rising", "Pierce Brown", 600, true);
+
+DisplayBooks();
